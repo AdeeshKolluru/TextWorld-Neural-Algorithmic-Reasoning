5407cade2317e9400cfc217da0a0cd5e80a85801
danielplatt <d.platt@web.de>
2021-07-13 12:17:58 +0100

    Add 'contained_in_shortest_path' label to maze edges

--- coincollector_mazes/get_maze.py
+++ coincollector_mazes/get_maze.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from textworld.challenges.tw_coin_collector.coin_collector import make as make_coin_collector_game
+import textworld
 from textworld.generator.game import GameOptions
 
 
@@ -8,13 +9,29 @@ def build_game_options(seed: int) -> GameOptions:
     options.seeds = seed
     return options
 
+def add_shortest_path_info(G: nx.DiGraph, game: textworld.Game) -> nx.DiGraph:
+    # label all edges as not contained in shortest path
+    for edge in G.edges:
+        G.add_edge(edge[0], edge[1], contained_in_shortest_path=0)
+
+    for node in G.nodes.items():
+        if node[1]['is_starting_position'] == 1:
+            starting_node = node[0]
+        if node[1]['has_coin'] == 1:
+            destination_node = node[0]
+    print(starting_node)
+    print(destination_node)
+    shortest_path = nx.shortest_path(G, source=starting_node, target=destination_node)
+    print(shortest_path)
+    for k in range(len(shortest_path)-1):
+        G.add_edge(shortest_path[k], shortest_path[k+1], contained_in_shortest_path=1)
+    return G
 
 def get_maze(level: int, seed: int) -> nx.DiGraph:
     options = build_game_options(seed)
     game = make_coin_collector_game({'level': level}, options)
 
     G = nx.DiGraph()
-
 
     for fact in game.world.facts:
         if fact.name == 'at' and fact.names[0] == 'P':
@@ -51,6 +68,8 @@ def get_maze(level: int, seed: int) -> nx.DiGraph:
             elif fact.name == 'west_of':
                 direc = 3
             G.add_edge(fact.names[0], fact.names[1], direction=direc)
+
+    G = add_shortest_path_info(G, game)
 
     return G
 
