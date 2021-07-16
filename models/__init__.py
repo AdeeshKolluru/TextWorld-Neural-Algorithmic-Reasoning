24a63601ee9339e5cf93696bccd05edad808f00b
yobibyte <vitaliykurin@gmail.com>
2021-07-16 11:02:19 +0100

    Add model to init.

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,5 @@ from .algorithm_base import AlgorithmBase
 from .augmenting_path_network import AugmentingPathNetwork
 from .bfs_network import BFSNetwork
 from .augmenting_path_network import AugmentingPathNetwork
-from .bf_network import BellFordNetwork
+from .bf_network import BellFordNetwork, NaturalBellFordNetwork
 from .maze_network import MazeNetwork
