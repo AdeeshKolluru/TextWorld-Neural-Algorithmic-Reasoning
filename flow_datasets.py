49cfa3de904ea029cd779248b882198f8cdd71a2
Davide Buffelli <dav.buffelli@gmail.com>
2021-07-16 08:16:09 +0200

    minor

--- flow_datasets.py
+++ flow_datasets.py
@@ -481,8 +481,8 @@ class MazeDataset(Dataset):
             return graph
         else:
             mapping = {"r_0": start_node, start_node: "r_0"}
-            new_is_starting_position = {"r_0": {"is_starting_position": 1}, start_node: {"is_starting_position": 0}}
-            nx.set_node_attributes(graph, new_is_starting_position)
+            #new_is_starting_position = {"r_0": {"is_starting_position": 1}, start_node: {"is_starting_position": 0}}
+            #nx.set_node_attributes(graph, new_is_starting_position)
             graph = nx.relabel_nodes(graph, mapping)
             return graph
 
@@ -507,7 +507,7 @@ class MazeDataset(Dataset):
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
             if self.split == "train":
-                number_of_graphs = 10
+                number_of_graphs = 100
                 seed = 100
             elif self.split == "val":
                 number_of_graphs = 5
